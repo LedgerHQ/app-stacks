4bc2b6e945f18454aca6ceb48cb3a2890d9bf90a
MadSquirrel <benoit.forgette@ci-yow.com>
2021-11-25 11:09:34 +0100

    [DEVELOP] Add minimal fuzzer

---
+++ fuzzing/fuzz_app_blockstack.c
@@ -0,0 +1,17 @@
+#include "parser.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    parser_tx_t txObj;
+    parser_context_t ctx;
+    parser_error_t rc;
+
+    rc = parser_parse(&ctx, data, size);
+    if (rc != parser_ok) {
+        return 0;
+    }
+    return 0;
+}
+
